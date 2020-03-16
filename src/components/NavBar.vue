9b45eae30bb47bc01f8b46bbc0f5a7b47236445a
jlozan64 <janeth2.jl@gmail.com>
2020-03-16 18:36:43 -0500

    fixed login

---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app-bar app clipped-left dark color="purple">
+    <v-app-bar-nav-icon
+      v-if="user"
+      @click="drawer ? hideDrawer() : showDrawer()"
+    />
+    <v-toolbar-title class="font-weight-light">
+      <span>Movies</span>
+      <span class="font-weight-regular">DB</span>
+    </v-toolbar-title>
+  </v-app-bar>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+export default {
+  name: "NavBar",
+  computed: {
+    ...mapGetters({
+      drawer: "getDrawer",
+      user: "getUser"
+    })
+  },
+  methods: {
+    ...mapActions(["hideDrawer", "showDrawer"])
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <v-container>
+      <v-row>
+        <v-col v-for="movie in movies" :key="movie.id" cols="3">
+          <MovieCard :movie="movie" :imageURL="imageURL" />
+        </v-col>
+      </v-row>
+      <div class="text-center">
+        <v-pagination
+          v-model="page"
+          color="purple"
+          :length="totalPages"
+          @input="getMovies"
+        />
+      </div>
+    </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import MovieCard from "@/components/MovieCard.vue";
 export default {
   name: "Home",
+  data() {
+    return {
+      movies: [],
+      certifications: [],
+      page: 1,
+      totalPages: 0,
+      configuration: {}
+    };
+  },
+  computed: {
+    imageURL() {
+      return `${this.configuration.secure_base_url}${
+        this.configuration.poster_sizes[
+          this.configuration.poster_sizes.length - 1
+        ]
+      }`;
+    }
+  },
+  created() {
+    this.getMovies();
+  },
   components: {
-    HelloWorld
+    MovieCard
+  },
+  methods: {
+    async getMovies() {
+      const movies = await this.$axios(
+        `discover/movie?page=${this.page}&certification_country=US&certification=G|PG|PG-13`
+      );
+      const certifications = await this.$axios("certification/movie/list");
+      const configuration = await this.$axios("configuration");
+      this.movies = movies.data?.results;
+      this.totalPages = movies.data?.total_pages;
+      this.certifications = certifications.data?.certifications.US;
+      this.configuration = configuration.data?.images;
+    }
   }
 };
 </script>
